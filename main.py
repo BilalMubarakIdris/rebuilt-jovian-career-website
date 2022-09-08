02fba2c154bc0c8a4a6098db26f62ba0e18465aa
Bilal Mubarak Idris <50290637+CodeWithBilal@users.noreply.github.com>
2022-09-08 18:56:17 +0000

    added image and manipulation of them through the project

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
+}]
 
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html", jobs=JOBS, company_name="Jovian")
+
+
+@app.route('/jobs/data_analyst')
+def data_analyst():
+    return render_template("data_analyst.html")
+
+
+@app.route('/api/jobs')
+def jobs_api():
+    return jsonify(JOBS)
 
 
 if __name__ == '__main__':
